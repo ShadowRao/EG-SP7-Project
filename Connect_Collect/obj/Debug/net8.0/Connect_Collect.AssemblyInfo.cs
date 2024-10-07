2d88e8b6f7c3049a705ee82bbda6a3409547ab21
manasabharadwaj <manasa2922x@gmail.com>
2024-10-07 12:19:53 +0530

    Checkbox and Re-entering password for customer signup

--- Connect_Collect/obj/Debug/net8.0/Connect_Collect.AssemblyInfo.cs
+++ Connect_Collect/obj/Debug/net8.0/Connect_Collect.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Connect_Collect")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de4c7c4005f65bbd73ed66777c16486cb8267f2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+288456cf9866752bd9681580c4b6fc9a544e458e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Connect_Collect")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Connect_Collect")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
