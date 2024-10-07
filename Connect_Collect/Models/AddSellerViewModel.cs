288456cf9866752bd9681580c4b6fc9a544e458e
Suhas S Gowda <sugow@eg.dk>
2024-10-07 11:55:39 +0530

    Modified SellerSignUp

--- Connect_Collect/Models/AddSellerViewModel.cs
+++ Connect_Collect/Models/AddSellerViewModel.cs
@@ -5,16 +5,18 @@ namespace Connect_Collect.Models
     public class AddSellerViewModel
     {
         public Guid SellerId { get; set; }
-        [Required(ErrorMessage = "Name is required")]
 
+        [Required(ErrorMessage = "Name is required")]
         [MaxLength(100)]
         public string? SellerName { get; set; }
 
         [EmailAddress]
         [Required(ErrorMessage = "Email is required")]
         public string? Email { get; set; }
+
         [Required(ErrorMessage = "Password is required")]
         public required string Password { get; set; }
+
         [Required(ErrorMessage = "Contact number is required")]
         public string? Contact { get; set; }
     }
