f488f820eb5e4eef4455a8dafacd43d1612fdca0
Shrao-01 <shrao@eg.dk>
2024-12-30 14:31:39 +0530

    New changes

--- Connect_Collect/obj/Debug/net8.0/Connect_Collect.AssemblyInfo.cs
+++ Connect_Collect/obj/Debug/net8.0/Connect_Collect.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Connect_Collect")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+274b413fb6ad2f05ac1467d67998c68708dd0206")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0416448ddb2ebbeac796579fb5a85638d768bb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Connect_Collect")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Connect_Collect")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
